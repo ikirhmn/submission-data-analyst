681c899d47a7c74fd2c85c2f571c665ce47b0da3
PutuIndah <puspitadewi967@gmail.com>
2025-03-09 23:21:45 +0800

    data path update

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -4,8 +4,8 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 # Load dataset
-day_df = pd.read_csv("data\day.csv")
-hour_df = pd.read_csv("data\hour.csv")
+day_df = pd.read_csv("day.csv")
+hour_df = pd.read_csv("hour.csv")
 
 # Konversi kolom tanggal
 day_df["dteday"] = pd.to_datetime(day_df["dteday"])
